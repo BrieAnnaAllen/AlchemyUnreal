a56a1dac8d469340ad7f27dd6b9ac93badd58b41
BrieAnna <BrieAnna.Allen@gmail.com>
2018-11-06 17:09:26 -0600

    Current Player Movement
    Player Movement Base using only PC control

---
+++ PlayerMovement/Source/PlayerMovement/BPTris.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnPlayerMove.h"
+#include "BPTris.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYERMOVEMENT_API ABPTris : public APawnPlayerMove
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
