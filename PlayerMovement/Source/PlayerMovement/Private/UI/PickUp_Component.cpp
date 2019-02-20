69e734af4109534a2adc6befaf61540b51cb1f55
tess.olson <tess.olson@T3600-CRUSHER>
2019-02-20 11:19:52 -0600

    Fixed error

--- PlayerMovement/Source/PlayerMovement/Private/UI/PickUp_Component.cpp
+++ PlayerMovement/Source/PlayerMovement/Private/UI/PickUp_Component.cpp
@@ -10,7 +10,6 @@ UPickUp_Component::UPickUp_Component()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	hasObject = false;
 }
 
 
